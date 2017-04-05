38b917033b72034ed6c9928b718d74d35e3878c7
huzaus <siarhei.huzau@grandparade.co.uk>
2017-04-05 13:01:54 +0200

    Remove declaration

--- src/test/java/com/huzaus/examples/generics/ExamplesTest.java
+++ src/test/java/com/huzaus/examples/generics/ExamplesTest.java
@@ -9,10 +9,6 @@ public class ExamplesTest {
     @Test
     public void questionOne() {
         List<? extends Number> list = new ArrayList<>();
-        list = new ArrayList<Number>();
-        list = new ArrayList<Long>();
-        list = new ArrayList<Integer>();
-        list = new ArrayList<Byte>();
 
         //list.add()
     }
@@ -20,8 +16,6 @@ public class ExamplesTest {
     @Test
     public void questionTwo() {
         List<? super Number> list = new ArrayList<>();
-        list = new ArrayList<Number>();
-        list = new ArrayList<Object>();
 
         //list.add();
     }
@@ -29,9 +23,6 @@ public class ExamplesTest {
     @Test
     public void questionThree() {
         List<?> list = new ArrayList<>();
-        list = new ArrayList<Number>();
-        list = new ArrayList<Object>();
-        list = new ArrayList<String>();
 
 //        list.add();
     }
