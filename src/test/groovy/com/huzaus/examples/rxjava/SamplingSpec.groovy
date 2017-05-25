0736d0e5ffa921ddcd48bf87734359dad929a370
huzaus <4kc8iED123>
2017-05-25 21:16:47 +0200

    Implement test with test subscriber and test scheduler

--- src/test/groovy/com/huzaus/examples/rxjava/SamplingSpec.groovy
+++ src/test/groovy/com/huzaus/examples/rxjava/SamplingSpec.groovy
@@ -1,6 +1,9 @@
 package com.huzaus.examples.rxjava
 
-import io.reactivex.Observable
+import rx.Observable
+import rx.observers.TestSubscriber
+import rx.schedulers.Schedulers
+import rx.schedulers.TestScheduler
 import spock.lang.Specification
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS
@@ -12,107 +15,87 @@ class SamplingSpec extends Specification {
 
     def "Time stamp sampling example"() {
         given:
-            long startTime = System.currentTimeMillis()
-            Observable timestamp = Observable
-                    .interval(interval, MILLISECONDS)
-                    .timestamp()
-                    .map({ timestamp -> "${timestamp.time() - startTime}ms: ${(timestamp.value() + 1)}" })
+        long startTime = System.currentTimeMillis()
+        Observable timestamp = Observable
+                .interval(interval, MILLISECONDS)
+                .timestamp()
+//                .map({ timestamp -> "${timestamp.time() - startTime}ms: ${(timestamp.value() + 1)}" })
+                .map({ timestamp -> "${timestamp.timestampMillis - startTime}ms: ${(timestamp.value)}" })
 
         expect:
-            println 'Sample:'
-            timestamp
-                    .sample(1L, SECONDS)
-                    .take(5L)
-                    .blockingSubscribe({ println it })
+        println 'Sample:'
+        timestamp
+                .sample(1L, SECONDS)
+                .take(5)
+                .toBlocking()
+                .subscribe({ println it })
+//                .blockingSubscribe({ println it })
+    }
+
+    def "Time stamp sampling example with test scheduler and test subscriber"() {
+        given:
+        TestScheduler scheduler = Schedulers.test()
+        TestSubscriber subscriber = TestSubscriber.create()
+        Observable timestamp = Observable
+                .interval(interval, MILLISECONDS, scheduler)
+                .timestamp()
+//                .map({ timestamp -> "${timestamp.time() - startTime}ms: ${(timestamp.value() + 1)}" })
+                .map({ timestamp -> timestamp.value })
+
+        expect:
+        println 'Sample:'
+        timestamp
+                .sample(1L, SECONDS, scheduler)
+                .take(5)
+//                .toBlocking()
+                .subscribe(subscriber)
+
+        scheduler.advanceTimeBy(1L, SECONDS)
+        subscriber.assertNotCompleted()
+        subscriber.assertValue(98L)
+
+        scheduler.advanceTimeTo(5L, SECONDS)
+        subscriber.assertCompleted()
+        subscriber.assertValues(98L, 198L, 298L, 398L, 498L)
     }
 
     def "Time stamp throttle first example"() {
         given:
-            long startTime = System.currentTimeMillis()
-            Observable timestamp = Observable
-                    .interval(interval, MILLISECONDS)
-                    .timestamp()
-                    .map({ timestamp -> "${timestamp.time() - startTime}ms: ${(timestamp.value() + 1)}" })
+        long startTime = System.currentTimeMillis()
+        Observable timestamp = Observable
+                .interval(interval, MILLISECONDS)
+                .timestamp()
+                .map({ timestamp -> "${timestamp.timestampMillis - startTime}ms: ${(timestamp.value)}" })
+//                .map({ timestamp -> "${timestamp.time() - startTime}ms: ${(timestamp.value() + 1)}" })
 
         expect:
-            println 'Throttle First:'
-            timestamp
-                    .throttleFirst(1L, SECONDS)
-                    .take(5L)
-                    .blockingSubscribe({ println it })
+        println 'Throttle First:'
+        timestamp
+                .throttleFirst(1L, SECONDS)
+                .take(5)
+                .toBlocking()
+                .subscribe({ println it })
+//                .blockingSubscribe({ println it })
     }
 
     def "Time stamp window example"() {
         given:
-            long startTime = System.currentTimeMillis()
-            Observable timestamp = Observable
-                    .interval(interval, MILLISECONDS)
-                    .timestamp()
-                    .map({ timestamp -> "${timestamp.time() - startTime}ms: ${(timestamp.value() + 1)}" })
+        long startTime = System.currentTimeMillis()
+        Observable timestamp = Observable
+                .interval(interval, MILLISECONDS)
+                .timestamp()
+                .map({ timestamp -> "${timestamp.timestampMillis - startTime}ms: ${(timestamp.value)}" })
+//                .map({ timestamp -> "${timestamp.time() - startTime}ms: ${(timestamp.value() + 1)}" })
 
         expect:
-            println 'Sample:'
-            timestamp
-                    .window(1L)
-                    .take(5L)
-                    .blockingSubscribe({ println it })
+        println 'Window:'
+        timestamp
+                .window(1)
+                .flatMap({ it })
+                .take(5)
+                .toBlocking()
+                .subscribe({ println it })
+//                .blockingSubscribe({ println it })
     }
-//    def "Sampling example"() {
-//
-//        given: "Observable with names: "
-//            Observable names = Observable.just(
-//                    'Zuzanna', 'Lena', 'Julia',
-//                    'Maja',
-//                    'Zofia',
-//                    'Hanna', 'Amelia',
-//                    'Aleksandra',
-//                    'Alicja', 'Natalia')
-//
-//        and: "Observable with absolute delays: "
-//            Observable delays = Observable.just(
-//                    0.1, 0.5, 0.9,
-//                    1.1,
-//                    2.3,
-//                    3.4, 3.5,
-//                    4.6,
-//                    5.4, 5.8)
-//                    .map({ it * 1_000 as Long })
-//
-//        and: "Observable that emits names with delay:"
-//            Observable delayedNames =
-//                    withAbsoluteDelays(names, delays)
-//
-//
-//        expect:
-//            TestScheduler scheduler = new TestScheduler()
-//            TestSubscriber subscriber = TestSubscriber.create()
-//            delayedNames
-//                    .sample(1L, SECONDS)
-//                    .subscribeOn(scheduler)
-//                    .test()
-////                    .blockingSubscribe()
-//                    .subscribe({ println it })
-//
-//            scheduler.advanceTimeTo(10L, SECONDS)
-////            scheduler.advanceTimeTo(2L, SECONDS)
-////            subscriber.assertValue("Julia")
-////
-//    }
-//
-//
-//    static Observable withAbsoluteDelays(Observable observable, Observable delays) {
-//        observable
-//                .concatWith(delayedCompletion())
-//                .zipWith(delays,
-//                { name, milliseconds ->
-//                    Observable
-//                            .just(name)
-//                            .delay(milliseconds as long, MILLISECONDS)
-//                })
-//                .flatMap({ it })
-//    }
-//
-//    static Observable delayedCompletion() {
-//        Observable.empty().delay(2L, SECONDS)
-//    }
+
 }
